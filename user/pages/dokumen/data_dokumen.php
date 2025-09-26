501702e76eef98efe6130693d4a1c5dc9e24c914
Ulfa12345 <rohmahulfatur383@gmail.com>
2025-09-26 15:19:36 +0700

    d

--- user/pages/dokumen/data_dokumen.php
+++ user/pages/dokumen/data_dokumen.php
@@ -41,10 +41,10 @@ $result = $stmt->get_result();
                             //serasi/user/pages/dokumen/uploads
                             // Ambil path file untuk setiap row
 
-                            $server_path_sipa = $_SERVER['DOCUMENT_ROOT'] . 'user/pages/dokumen/uploads/' . basename($perusahaan['upload_sipa']);
+                            $server_path_sipa = $_SERVER['DOCUMENT_ROOT'] . 'pages/dokumen/uploads/' . basename($perusahaan['upload_sipa']);
                             $web_path_sipa = 'user/pages/dokumen/uploads/' . basename($perusahaan['upload_sipa']);
 
-                            $server_path_sph = $_SERVER['DOCUMENT_ROOT'] . 'user/pages/dokumen/uploads/' . basename($row['upload_suratpermohonan']);
+                            $server_path_sph = $_SERVER['DOCUMENT_ROOT'] . 'pages/dokumen/uploads/' . basename($row['upload_suratpermohonan']);
                             $web_path_sph = 'user/pages/dokumen/uploads' . basename($row['upload_suratpermohonan']);
 
                             $server_path_spn = $_SERVER['DOCUMENT_ROOT'] . '/serasi/user/pages/dokumen/uploads/' . basename($row['upload_suratpernyataan']);
